4d0d4bdf60023517f8e95830aa8cd5f951b68426
Rafael Maia <rafaeucaetanoquintas@gmail.com>
2024-03-09 16:50:49 -0300

    created target class

--- src/aimtrainer.py
+++ src/aimtrainer.py
@@ -7,8 +7,36 @@ pygame.init()
 
 WIDTH, HEIGHT = 800, 600
 
-win = pygame.display.set_mode((WIDTH, HEIGHT))
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Aim Trainer')
+
+
+class Target:
+    max_size = 30
+    growth_rate = 0.2
+    color = 'red'
+    second_color = 'white'
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.size = 0
+        self.grow = True
+
+    def update(self):
+        if self.size + self.growth_rate >= self.max_size:
+            self.grow = False
+
+        if self.grow:
+            self.size += self.growth_rate
+        else:
+            self.size -= self.growth_rate
+
+    def draw(self, win):
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.size)
+        pygame.draw.circle(win, self.second_color, (self.x, self.y), self.size * 0.8)
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.size * 0.6)
+        pygame.draw.circle(win, self.second_color, (self.x, self.y), self.size * 0.4)
 
 
 def main():
